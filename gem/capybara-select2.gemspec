2a6216fb9a274b9fccc8b69aad6c5aea9117fbe5
Leonardo Falk <leonardo.falk@hotmail.com>
2017-01-17 17:21:48 -0200

    Allow :id parameter and fix deprecated warning

--- gem/capybara-select2.gemspec
+++ gem/capybara-select2.gemspec
@@ -4,19 +4,19 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'capybara-select2/version'
 
 Gem::Specification.new do |gem|
-  gem.name          = "capybara-select2"
+  gem.name          = 'capybara-select2'
   gem.version       = Capybara::Select2::VERSION
-  gem.authors       = ["William Yeung"]
-  gem.email         = ["william@tofugear.com"]
-  gem.description   = %q{Helper for triggering select for select2 javascript library}
-  gem.summary       = ""
-  gem.homepage      = ""
+  gem.authors       = ['William Yeung']
+  gem.email         = ['william@tofugear.com']
+  gem.description   = 'Helper for triggering select for select2 javascript library'
+  gem.summary       = ''
+  gem.homepage      = ''
 
-  gem.files         = `git ls-files`.split($/)
-  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.files         = `git ls-files`.split($INPUT_RECORD_SEPARATOR)
+  gem.executables   = gem.files.grep(%r{^bin/}).map { |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ["lib"]
-  
+  gem.require_paths = ['lib']
+
   gem.add_dependency 'rspec'
   gem.add_dependency 'capybara'
 end
--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -1,47 +1,47 @@
-require "capybara-select2/version"
+require 'capybara-select2/version'
 require 'capybara/selectors/tag_selector'
 require 'rspec/core'
 
 module Capybara
   module Select2
     def select2(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath' or 'css'" unless options.is_a?(Hash) and [:from, :xpath, :css].any? { |k| options.has_key? k }
+      raise "Must pass a hash containing 'from' or 'xpath' or 'css' or 'id'" unless options.is_a?(Hash) && [:from, :xpath, :css, :id].any? { |k| options.key? k }
 
       @options = options
 
-      if options.has_key? :xpath
+      if options.key? :xpath
         select2_container = find(:xpath, options[:xpath], match: :first)
-      elsif options.has_key? :css
+      elsif options.key? :css
         select2_container = find(:css, options[:css], match: :first)
-      elsif options.has_key? :id
+      elsif options.key? :id
         select2_container = find_by_id(options[:id])
       else
         select_name = options[:from]
-        select2_container = find("label", text: select_name, match: :first).find(:xpath, '..').find(".select2-container")
+        select2_container = find('label', text: select_name, match: :first).find(:xpath, '..').find(:css, '.select2-container', match: :first)
       end
 
       # Open select2 field
-      if select2_container.has_selector?(".select2-selection")
+      if select2_container.has_selector?('.select2-selection')
         # select2 version 4.0
-        select2_container.find(".select2-selection").click
-      elsif select2_container.has_selector?(".select2-choice")
-        select2_container.find(".select2-choice").click
+        select2_container.find('.select2-selection').click
+      elsif select2_container.has_selector?('.select2-choice')
+        select2_container.find('.select2-choice').click
       else
-        select2_container.find(".select2-choices").click
+        select2_container.find('.select2-choices').click
       end
 
-      if options.has_key? :search
-        find(:xpath, "//body").find(".select2-search input.select2-search__field").set(value)
+      if options.key? :search
+        find(:xpath, '//body').find('.select2-search input.select2-search__field').set(value)
         page.execute_script(%|$("input.select2-search__field:visible").keyup();|)
-        @drop_container = ".select2-results"
-      elsif find(:xpath, "//body").has_selector?(".select2-dropdown")
+        @drop_container = '.select2-results'
+      elsif find(:xpath, '//body').has_selector?('.select2-dropdown')
         # select2 version 4.0
         unless @options[:without_searching]
-          find(:xpath, "//body").find(".select2-search.select2-search--dropdown input.select2-search__field").set(value)
+          find(:xpath, '//body').find('.select2-search.select2-search--dropdown input.select2-search__field').set(value)
         end
-        @drop_container = ".select2-dropdown"
+        @drop_container = '.select2-dropdown'
       else
-        @drop_container = ".select2-drop"
+        @drop_container = '.select2-drop'
       end
 
       [value].flatten.each do |value|
@@ -51,46 +51,46 @@ module Capybara
 
     private
 
-      def select_option(value)
-        clicked = wait_for_option_with_text(value)
-        unless clicked
-          click_on_option(value)
-        end
-      end
+    def select_option(value)
+      clicked = wait_for_option_with_text(value)
+      click_on_option(value) unless clicked
+    end
 
-      def select2_option_selector
-        if find(:xpath, "//body").has_selector?("#{@drop_container} li.select2-results__option")
-          "#{@drop_container} li.select2-results__option"
-        else
-          "#{@drop_container} li.select2-result-selectable"
-        end
+    def select2_option_selector
+      if find(:xpath, '//body').has_selector?("#{@drop_container} li.select2-results__option")
+        "#{@drop_container} li.select2-results__option"
+      else
+        "#{@drop_container} li.select2-result-selectable"
       end
+    end
 
-      def wait_for_option_with_text(value)
-        clicked = false
-        begin
-          Timeout.timeout(2) do
-            if page.has_selector?(select2_option_selector, text: value)
-              click_on_option(value)
-              clicked = true
-              break
-            else
-              sleep(0.1)
-            end
+    def wait_for_option_with_text(value)
+      clicked = false
+
+      begin
+        Timeout.timeout(2) do
+          if page.has_selector?(select2_option_selector, text: value)
+            click_on_option(value)
+            clicked = true
+            break
+          else
+            sleep(0.1)
           end
-        rescue TimeoutError
         end
-        clicked
+      rescue Timeout::Error
+        return clicked
       end
 
-      def click_on_option(value)
-        if @options[:first] == true
-          find(:xpath, "//body").first(select2_option_selector, text: value).click
-        else
-          find(:xpath, "//body").find(select2_option_selector, text: value).click
-        end
-      end
+      clicked
+    end
 
+    def click_on_option(value)
+      if @options[:first] == true
+        find(:xpath, '//body').first(select2_option_selector, text: value).click
+      else
+        find(:xpath, '//body').find(select2_option_selector, text: value).click
+      end
+    end
   end
 end
 
